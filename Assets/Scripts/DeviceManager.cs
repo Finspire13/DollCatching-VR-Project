94a1e282987ba7beb85d731e870b2be025f889dd
zhoukaimao <zhoukaimao@163.com>
2016-06-07 22:16:11 +0800

    DeviceManager
    Add eye position and brain value management.
    And doll catch machine model

---
+++ Assets/Scripts/DeviceManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(GazePointDataComponent))]
+public class DeviceManager : MonoBehaviour {
+    Vector2 gazePoint;
+    int brainValue;
+    TGCConnectionController controller;
+    private int attention1;
+	// Use this for initialization
+	void Start () {
+
+        gazePoint = new Vector2();
+
+        controller = GameObject.Find("NeuroSkyTGCController").GetComponent<TGCConnectionController>();
+        controller.UpdateAttentionEvent += OnUpdateAttention;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        brainValue = attention1;
+	    // Get the last gaze point.     
+        var lastGazePoint =  GetComponent<GazePointDataComponent>().LastGazePoint;
+        if(lastGazePoint.IsWithinScreenBounds)     
+        {         
+            // Get the point in screen space coordinates.         
+            gazePoint = lastGazePoint.Screen;    
+        }
+	}
+    public float GetBrainValue()
+    {
+        return brainValue;
+    }
+    public Vector2 GetGazePoint()
+    {
+        return gazePoint;
+    }
+    void OnUpdateAttention(int value)
+    {
+        attention1 = value;
+    }
+}
